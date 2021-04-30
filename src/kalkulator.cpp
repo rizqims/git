9ee7ae1edd4aee65ef5fd4abcd5ff163c56fd283
Maves_Dev <83437849+MavesDev@users.noreply.github.com>
2021-04-30 14:16:54 +0700

    Menambah module 4 Petinggi perhitungan dan UI
    Menambah kan fitur :
    - Perkalian
    - Pertambahan
    - Perkalian
    - Pengurangan
    Dan sedikit memperindah UI

--- src/kalkulator.cpp
+++ src/kalkulator.cpp
@@ -20,47 +20,88 @@ public:
         return bil1 / bil2;
     }
 
-    //TODO buat function penjumlahan
-    //TODO buat function pengurangan
     //TODO buat function perkalian
+    int perkalian()
+    {
+        return bil1 * bil2;
+    }
+    
+	//TODO buat function penjumlahan
+    int penjumlahan()
+    {
+        return bil1 + bil2;
+    }
+    
+	//TODO buat function pengurangan
+    int pengurangan()
+    {
+        return bil1 - bil2;
+    }
 };
 
 int main(int argc, char const *argv[])
 {
+	cout << "============================" << endl;
     cout << "Program Kalkulator sederhana" << endl;
+    cout << "============================" << endl;
+    
     // deklarasi variabel untuk bilangan 1 dan 2 juga op
     int a, b;
-    char op;
+    char op, pil;
+    
+    // Label untuk kembali
+    balik:
+    
+    // Sistem Input 
+    cout << "\n=======Masukan Angka========" << endl;
     cout << "Bilangan pertama : ";
     cin >> a;
     cout << "Bilangan kedua : ";
     cin >> b;
     Kalkulator Kalkulator1 = Kalkulator(a, b);
-
-// untuk balik bisi ada kesalahan penulisan operator
-balik:
-    cout << "op :";
+    cout << "\n[(+) (-) (*) (/)]" << endl;
+    cout << "Operasi :";
     cin >> op;
-    switch (op)
-    {
-    case '+':
-
-        break;
-    case '-':
-
-        break;
-    case '*':
-
-        break;
-    case '/':
-        cout << "hasilnya adalah : " << Kalkulator1.pembagian() << endl;
-        break;
-
-    default:
-        cout << "mohon masukkan operator yang tepat" << endl;
-        goto balik;
-        break;
+    cout << "============================" << endl;
+     
+    // Sistem Output
+    cout << "\n===========Hasil============" << endl;
+     if (op == '+')
+     {
+         cout << "hasilnya adalah :  " << Kalkulator1.penjumlahan() << endl;
+     }
+     else if (op == '-')
+     {
+         cout << "hasilnya adalah :  " << Kalkulator1.pengurangan() << endl;
+     }
+     else if (op == '*')
+     {
+         cout << "hasilnya adalah :  " << Kalkulator1.perkalian() << endl;
+     }
+     else if (op == '/')
+     {
+         cout << "hasilnya adalah : " << Kalkulator1.pembagian() << endl;
+     }
+     else
+     {
+         cout << "mohon masukkan operator yang benar" << endl;
+         goto balik;
     }
-    return 0;
-    // a
+    cout << "============================" << endl;
+    
+    // Sistem validasi exit
+    cout << "\nKembali? [y/n]" << endl;
+    cin >> pil;
+    
+    if(pil == 'y')
+	{
+		goto balik;
+	}
+	else if(pil == 'n')
+	{
+		cout << "\nThank You" << endl;
+		return 0;	
+	}
+
+    
 }
